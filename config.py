372a765bdb99eadd940b7a166a7d197dc1ed5d77
Bri Prebilic Cole <b_prebiliccole@u.pacific.edu>
2018-12-05 17:10:33 -0800

    downloaded Berkeley's native flora database for tree comparison using BeautifulSoup

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 TREES_GEOJSON_URL = 'https://opendata.arcgis.com/datasets/b9b716e09b5048179ab648bb4518452b_0.geojson'
 
-FLORA_URL = 'http://ucjeps.berkeley.edu/interchange/'
+FLORA_URL = 'http://ucjeps.berkeley.edu/flora_index.html'
+FLORA_A_TAG_IDENTIFIER = 'http://bscit.berkeley.edu/cgi-bin/display_page?'
 
 
 dir_path, filename = os.path.split(os.path.realpath(__file__))
